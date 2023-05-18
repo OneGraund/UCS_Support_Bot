50f14cd0d09ad13ad894970a9de5cc0e5f8bac6a
unknown <vovangaunder@gmail.com>
2023-05-18 12:00:06 +0200

    Added calling of a function in tg_bot from src.utilities.helpers, changed functions in src.google_sheets.api to see whether functions are beeing called

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from os import getenv
 from time import sleep
 import threading
+import src.openai_gpt.gpt
+import src.utilities.helpers
 
 
 class UCSSupportBot:
@@ -30,9 +32,39 @@ class UCSSupportBot:
             self.send_message(self.group_id, '[HANDLERS] Handlers initialised')
 
         @self.bot.message_handler(func=lambda message: True)
-        def echo_all(message):
+        def message_handling(message):
             # Handlers func goes here
-            self.bot.reply_to(message, message.text)
+            if message.text.lower().find('bot')!=-1 or message.text.lower().find('бот')!=-1:
+                response = src.openai_gpt.gpt.choose_command(
+                    available_functions=src.utilities.helpers.available_functions,
+                    text=message.text
+                )
+                if response.find(';')!=-1:
+                    response = response.split(';')
+                    to_send=f'Calling function {response[0]}. With arguments: '
+                    for i in range(1, len(response)):
+                        to_send+=f'{response[i]} '
+                    self.send_message(
+                        chat_id=self.group_id,
+                        message=to_send
+                    )
+                    to_print = f'[TELEGRAM] Message text: {message.text}\n\t[OPENAI CHOOSE_COMMAND]' \
+                               f' chose function {response[0]} with arguments: '
+                    for index, arg in enumerate(response[1:]):
+                        to_print+= f'{arg}, '
+                        response[index+1]=response[index+1][1:]
+                    print(to_print)
+                    src.utilities.helpers.available_functions[response[0]]['func'](response[1:])
+                else:
+                    self.send_message(
+                        chat_id=self.group_id,
+                        message=f'Calling function: {response}'
+                    )
+                    print(f'[TELEGRAM] Calling function: {response}')
+                    src.utilities.helpers.available_functions[response]['func']()
+            else:
+                print('[TELEGRAM] A message was spotted in group chat, but not to bot')
+                self.bot.reply_to(message, message.text)
 
         if UCSSupportBot.DEBUG:
             self.bot.polling()
@@ -55,7 +87,7 @@ if __name__ == '__main__':
         GROUP_CHAT_ID = getenv('TELEGRAM_TEST_GROUP_ID')
     else:
         GROUP_CHAT_ID = getenv('TELEGRAM_UCS_GROUP_ID')
-    UCSSupportBot.DEBUG = 0
+    UCSSupportBot.DEBUG = 1
     UCSSupportBot(
         token=TELEGRAM_TOKEN,
         ucs_group_chat_id=GROUP_CHAT_ID
